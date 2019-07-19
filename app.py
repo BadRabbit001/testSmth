c642f52cce7251a16b95453deec1bc8821ef1674
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-07-19 14:50:01 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import discord
 from discord.ext import commands, tasks
 import os
 import asyncio
+import random
 from itertools import cycle
 
 #TOKEN = process.env.BOT_TOKEN
@@ -16,6 +17,7 @@ status = cycle(['with the &help command.', 'with the developers console', 'with 
 
 @client.event
 async def on_message(message):
+    author = '{0.author.mention}'.format(message)
     # we do not want the bot to reply to itself
     if message.author == client.user:
         return
@@ -23,75 +25,17 @@ async def on_message(message):
     if message.content.startswith('!hello'):
         msg = 'Hello python {0.author.mention}'.format(message)
         await message.channel.send(msg)
-@client.event
-async def on_message(message):
-    if message.content.startswith('!help'):
-        await client.send_message(message.channel,'<@%s> visit #how-to-gen channel'  %(message.author.id))
-    if message.content.startswith('!minecraft'):
-        randomlist1 = ["https://link-to.net/27527/Minecraft001","https://up-to-down.net/27527/minecrafts","https://filemedia.net/27527/Minecraft"]
-        await client.send_message(message.author,"Your link to Minecraft accounts: " + (random.choice(randomlist1)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!nitro'):
-        randomlist2 = ["We are sorry but we are currently out of Nitro codes."]
-        await client.send_message(message.author,"Your link to Nitro codes: " + (random.choice(randomlist2)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content == '!spotify':
-        randomlist3 = ["https://direct-link.net/27527/spotify4","https://direct-link.net/27527/spotify2"]
-        await client.send_message(message.author,"Your link to Spotify accounts: " + (random.choice(randomlist3)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!nord'):
-        randomlist4 = ["https://filemedia.net/27527/NordVPN"]
-        await client.send_message(message.author,"Your link to NordVPN accounts: " + (random.choice(randomlist4)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!origin'):
-        randomlist5 = ["https://link-to.net/27527/origin","https://up-to-down.net/27527/origin2","https://direct-link.net/27527/origin3"]
-        await client.send_message(message.author,"Your link to Origin accounts: " + (random.choice(randomlist5)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!hulu'):
-        randomlist6 = ["https://filemedia.net/27527/hulu","https://filemedia.net/27527/hulu2"]
-        await client.send_message(message.author,"Your link to Hulu accounts: " + (random.choice(randomlist6)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!steam'):
-        randomlist7 = ["https://filemedia.net/27527/steam"]
-        await client.send_message(message.author,"Your link to Steam accounts: " + (random.choice(randomlist7)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!pinterest'):
-        randomlist8 = ["https://link-to.net/27527/pinterest"]
-        await client.send_message(message.author,"Your link to Pinterest accounts: " + (random.choice(randomlist8)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!udemy'):
-        randomlist9 = ["https://up-to-down.net/27527/udemy","https://filemedia.net/27527/udemy2"]
-        await client.send_message(message.author,"Your link to Udemy accounts: " + (random.choice(randomlist9)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!uplay'):
-        randomlist10 = ["https://up-to-down.net/27527/uplay2","https://up-to-down.net/27527/uplay"]
-        await client.send_message(message.author,"Your link to Uplay accounts: " + (random.choice(randomlist10)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!crunchyroll'):
-        randomlist11 = ["https://up-to-down.net/27527/crunchyroll"]
-        await client.send_message(message.author,"Your link to Crunchyroll accounts: " + (random.choice(randomlist11)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!scribd'):
-        randomlist12 = ["https://direct-link.net/27527/Scribd"]
-        await client.send_message(message.author,"Your link to Scribd accounts: " + (random.choice(randomlist12)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!grammarly'):
-        randomlist13 = ["https://direct-link.net/27527/grammarly"]
-        await client.send_message(message.author,"Your link to Grammarly accounts: " + (random.choice(randomlist13)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!fuckbook'):
-        randomlist14 = ["https://filemedia.net/27527/fcbbook"]
-        await client.send_message(message.author,"Your link to Fuckbook accounts: " + (random.choice(randomlist14)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith('!familyowner'):
-        randomlist15 = ["https://direct-link.net/27527/familyowner"]
-        await client.send_message(message.author,"Your link to Familyowner accounts: " + (random.choice(randomlist15)))
-        await client.send_message(message.channel,"Check your DM's")
-    if message.content.startswith("!spotifygen"):
-        randomlist16 = ["https://direct-link.net/27527/SpotifyGenerator001"]
-        await client.send_message(message.author,"Your link to download Spotify Generator is ready: " + (random.choice(randomlist16)))
-        await client.send_message(message.channel,"Check your DM's")
+
+    if message.content.startswith('!fortnite'):
+        randomlist = ['opt1','opt2','opt3']
+        msg = 'Hello ' + author + '. Your link: '
 		
+	if message.content.startswith('!nord'):
+        randomlist = ['opt1','opt2','opt3']
+        msg = 'Hello ' + author + '. Your link: '
+        await message.channel.send(msg + (random.choice(randomlist)))
+        await message.channel.send(msg + (random.choice(randomlist)))
+
 @client.event
 async def on_ready():
     print('Logged in as')
