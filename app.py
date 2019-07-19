1fa0358c074fb8de361106d00ce83cb1d3a33773
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-07-19 14:35:37 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,31 +1,28 @@
+# Work with Python xxx
 import discord
-from discord.ext.commands import Bot
-from discord.ext import commands
+from discord.ext import commands, tasks
 import os
 import asyncio
-import time
-import random
-from discord import Game
+from itertools import cycle
 
-Client = discord.client
-client = commands.Bot(command_prefix = '!')
-Clientdiscord = discord.Client()
+#TOKEN = process.env.BOT_TOKEN
+#TOKEN = 'NjAwMjgyODA2MzQxNDY4MTYx.XSxhug.CdE5SULghOlZyKIg7GvCmbxm-z8'
 
+client = commands.Bot(command_prefix='.')
+#client = discord.Client()
+
+#create an arraylist containing phrases you want your bot to switch through.
+status = cycle(['with the &help command.', 'with the developers console', 'with some code', 'with JavaScript'])
 
 @client.event
-async def on_ready():
-    print("Bot Was Deployed Sucessfully !")
-    while True:
-        await client.change_presence(game=Game(name='with BadRabbit'))
-        await asyncio.sleep(3)
-        await client.change_presence(game=Game(name='with Generator'))
-        await asyncio.sleep(3)
-        await client.change_presence(game=Game(name='this Server', type = 3))
-        await asyncio.sleep(3)
-        await client.change_presence(game=Game(name='Viktor Sheen', type = 2))
-        await asyncio.sleep(3)
+async def on_message(message):
+    # we do not want the bot to reply to itself
+    if message.author == client.user:
+        return
 
-
+    if message.content.startswith('!hello'):
+        msg = 'Hello python {0.author.mention}'.format(message)
+        await message.channel.send(msg)
 @client.event
 async def on_message(message):
     if message.content.startswith('!help'):
@@ -98,7 +95,17 @@ async def on_message(message):
         randomlist16 = ["https://direct-link.net/27527/SpotifyGenerator001"]
         await client.send_message(message.author,"Your link to download Spotify Generator is ready: " + (random.choice(randomlist16)))
         await client.send_message(message.channel,"Check your DM's")
+		
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+    change_status.start()
 
+@tasks.loop(seconds=10)
+async def change_status():
+    await client.change_presence(activity=discord.Game(next(status)))
 
-
-client.run(os.getenv("BOT_TOKEN"))
+client.run(os.getenv('BOT_TOKEN'))
