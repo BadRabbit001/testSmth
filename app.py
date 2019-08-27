4ee07f6f60130ffca04a2a6a72da72520a6e1e69
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-08-27 14:24:34 +0200

    Update app.py

--- app.py
+++ app.py
@@ -12,6 +12,15 @@ import os
 
 client = commands.Bot(command_prefix='!')
 #client = discord.Client()
+Clientdiscord = discord.Client()
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('!help'):
+        embed = discord.Embed(title="For more info visit http://rabbit001.cf", description="", color=0x00ff00)
+        embed.add_field(name="commands:", value="for list of commands visit: http://rabbit001.cf/commands.html", inline=False)
+        embed.add_field(name="U want invite my bot to ur server? Use this link:", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        await message.channel.send(message.channel, embed=embed)
 
 #create an arraylist containing phrases you want your bot to switch through.
 status = cycle(['www.rabbit001.cf', 'With BlackRabbit', 'with Generator', 'with accounts', '!invite'])
