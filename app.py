5d95da398ddadf8dab0a924247ce281c1401a874
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-08-27 14:26:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -41,7 +41,7 @@ async def ban(ctx):
  
 @client.event
 async def on_message(message):
-    if message.content.startswith('!help'):
+    if message.content.startswith('!helpME'):
         embed = discord.Embed(title="For more info visit http://rabbit001.cf", description="", color=0x00ff00)
         embed.add_field(name="commands:", value="for list of commands visit: http://rabbit001.cf/commands.html", inline=False)
         embed.add_field(name="U want invite my bot to ur server? Use this link:", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
@@ -156,9 +156,6 @@ async def on_message(message):
         randomlist = ['https://link-to.net/27527/Minecraft001','https://up-to-down.net/27527/minecrafts','https://filemedia.net/27527/Minecraft']
         msg = 'Hello ' + author + '. Your link: '
         await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('!help'):
-        await message.author.send("https://rabbit001.cf/bot/commands.html")
         
         
     if message.content.startswith('!purge'):
