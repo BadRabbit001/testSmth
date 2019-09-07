ba442a241988aae26ff31c74ae66158213ce543d
BadRabbit001 <52998155+BadRabbit001@users.noreply.github.com>
2019-09-08 01:00:51 +0200

    Update app.py

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ status = cycle(['web: www.rabbit001.cf', 'With BlackRabbit', 'discord.gg/cZ8GcPF
 client.remove_command('help')
 
 @client.command()
-async def clear(ctx, amount=5):
+async def clr(ctx, amount=5):
     await ctx.channel.purge(limit=amount)
 
 @client.command()
@@ -34,129 +34,124 @@ async def ban(ctx):
 
 @client.event
 async def on_ready():
-    print("Bot Was Deployed Sucessfully !")
-    while True:
-        await client.change_presence(game=Game(name='with BadRabbit'))
-        await asyncio.sleep(3)
-        await client.change_presence(game=Game(name='with Generator'))
-        await asyncio.sleep(3)
-        await client.change_presence(game=Game(name='this Server', type = 3))
-        await asyncio.sleep(3)
-        await client.change_presence(game=Game(name='Viktor Sheen', type = 2))
-        await asyncio.sleep(3)
+    print("=======================================")
+    print()
+    print("Hello BlackRabbit#3981")
+    print("client id :", client.user.id)
+    print()
+    print("=======================================")
+    game = discord.Game("")
+    await client.change_presence(status=discord.Status.online, activity=game)
+
 
 
 @client.event
 async def on_message(message):
-    message.content = message.content.lower()
-    author = '{0.author.mention}'.format(message)
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello python {0.author.mention}'.format(message)
-        await message.author.send(msg)
-        
-
-    if message.content.startswith('!invite'):
-        await message.author.send("For invite link visit http://rabbit001.cf/")
-        
-    if message.content.startswith('!fortnite'):
-        randomlist = ['https://filemedia.net/27527/fortnite','https://up-to-down.net/27527/fortnite02','https://filemedia.net/27527/fortnite2']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('?ban'):
-        msg = 'https://gifimage.net/wp-content/uploads/2017/07/ban-hammer-gif-14.gif'.format(message)
-        await message.channel.send(msg)
-                
-    if message.content.startswith('!Spotify'):
-        randomlist = ['https://direct-link.net/27527/spotify2','https://direct-link.net/27527/spotify4','https://direct-link.net/27527/spotify2']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('rabbit'):
-        msg = 'https://i.pinimg.com/originals/ea/5b/b4/ea5bb42b167972d4121152caded1bcf4.gif'.format(message)
-        await message.channel.send(msg)  
-            
-    if message.content.startswith('!stock'):
-        randomlist = ['visit #how-to-gen for commands','visit #how-to-gen for commands','visit #how-to-gen for commands']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('!nord'):
-        randomlist = ['https://filemedia.net/27527/NordVPN','https://filemedia.net/27527/NordVPN','https://filemedia.net/27527/NordVPN']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('!spotify'):
-        randomlist = ['https://direct-link.net/27527/spotify4','https://direct-link.net/27527/spotify4','https://direct-link.net/27527/spotify3']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-
-    if message.content.startswith('!origin'):
-        randomlist = ['https://link-to.net/27527/origin','https://link-to.net/27527/origin','https://link-to.net/27527/origin']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-                
-    if message.content.startswith('!hulu'):
-        randomlist = ['https://filemedia.net/27527/hulu2','https://filemedia.net/27527/hulu','https://filemedia.net/27527/hulu2']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('!steam'):
-        randomlist = ['https://filemedia.net/27527/steam	','https://filemedia.net/27527/steam	','https://filemedia.net/27527/steam	']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('!udemy'):
-        randomlist = ['https://filemedia.net/27527/udemy2','https://up-to-down.net/27527/udemy','https://up-to-down.net/27527/udemy']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-                
-    if message.content.startswith('!uplay'):
-        randomlist = ['https://up-to-down.net/27527/uplay2','https://up-to-down.net/27527/uplay2','https://up-to-down.net/27527/uplay']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-    if message.content.startswith('!crunchyroll'):
-        randomlist = ['https://up-to-down.net/27527/crunchyroll','https://up-to-down.net/27527/crunchyroll','https://up-to-down.net/27527/crunchyroll']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-                
-    if message.content.startswith('!scribd'):
-        randomlist = ['https://direct-link.net/27527/Scribd','https://direct-link.net/27527/Scribd','https://direct-link.net/27527/Scribd']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-                        
-    if message.content.startswith('!familyowner'):
-        randomlist = ['https://direct-link.net/27527/familyowner','https://direct-link.net/27527/familyowner','https://direct-link.net/27527/familyowner']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-                                
-    if message.content.startswith('!minecraft'):
-        randomlist = ['https://link-to.net/27527/Minecraft001','https://up-to-down.net/27527/minecrafts','https://filemedia.net/27527/Minecraft']
-        msg = 'Hello ' + author + '. Your link: '
-        await message.author.send(msg + (random.choice(randomlist)))
-        
-        
-    if message.content.startswith('!purge'):
-        args = message.content.split(" ")
-        a = int(args[1])
-        await message.channel.purge(limit=a)
-    await client.process_commands(message)
-   
-@client.event
-async def on_message(message):
-    if message.content.startswith("!cmds"):
+    message.content = message.content.lower().replace(' ', '')
+    if message.content.startswith("!hello"):
         print(message.author.name)
-        author = message.guild.get_member(message.author.id)
-        embed = discord.Embed(color=0xFFCF20)
-        embed.add_field(name="Commands:", value="for list of commands visit http://rabbit001.cf/commands.html", inline=False)
-        embed.add_field(name="My server invite", value="For invite to my server visit http://rabbit001.cf", inline=False)
-        embed.add_field(name="Contact", value="DM ME! BlackRabbit#3981", inline=False)
-        embed.add_field(name="Invite this bot on server!", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        embed = discord.Embed(color=0xFF09D7)
+        embed.add_field(name="text", value="https://direct-link.net/27527/spotify3", inline=False)
+        embed.add_field(name="Link #2:", value="https://direct-link.net/27527/spotify4", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+    if message.content.startswith("!nord"):
+        print(message.author.name)
+        embed = discord.Embed(title="`NordVPN acc`", color=0xFF09D7)
+        embed.add_field(name="Your link:", value="https://filemedia.net/27527/NordVPN", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)     
+
+        
+    if message.content.startswith("!minecrafts"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Minecraft acc`", color=0x400cc1)
+        embed.add_field(name="Your link:", value="https://link-to.net/27527/Minecraft001", inline=False)
+        embed.add_field(name="Link #2:", value="https://filemedia.net/27527/Minecraft", inline=False)
+        embed.add_field(name="Link #3:", value="https://up-to-down.net/27527/minecrafts", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+
+    if message.content.startswith("!hulu"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Hulu acc`", color=0x40cc55)
+        embed.add_field(name="Your link:", value="https://filemedia.net/27527/hulu2", inline=False)
+        embed.add_field(name="Link #2:", value="https://filemedia.net/27527/hulu", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapp.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+        
+    if message.content.startswith("!origin"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Origin acc`", color=668000)
+        embed.add_field(name="Your link:", value="https://link-to.net/27527/Origin", inline=False)
+        embed.add_field(name="Your link:", value="https://direct-link.net/27527/Origin3", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordappf.com/oauth2/authorize?client_id=604967241863397376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+
+    if message.content.startswith("!uplay"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Uplay acc`", color=0x666644)
+        embed.add_field(name="Your link:", value="https://up-to-down.net/27527/uplay", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+
+    if message.content.startswith("!fortnite"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Fortnite acc`", color=0x296666)
+        embed.add_field(name="Your link:", value="https://filemedia.net/27527/fortnite", inline=False)
+        embed.add_field(name="Your link:", value="https://filemedia.net/27527/fortnite2", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+    if message.content.startswith("!steam"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Steam acc`", color=0x796666)
+        embed.add_field(name="Your link:", value="https://filemedia.net/27527/Steam", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed) 
+        
+    if message.content.startswith("!spotify"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Spotify acc`", color=0x996666)
+        embed.add_field(name="Your link:", value="https://direct-link.net/27527/spotify4", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed) 
+        
+    if message.content.startswith("!udemy"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Udemy acc`", color=0x550000)
+        embed.add_field(name="Your link:", value="https://filemedia.net/27527/udemy2", inline=False)
+        embed.add_field(name="Your link:", value="https://up-to-down.net/27527/udemy", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+    if message.content.startswith("!scribd"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Scrib acc`", color=0x840055)
+        embed.add_field(name="Your link:", value="https://direct-link.net/27527/Scribd", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+    if message.content.startswith("!pinterest"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Pinterest acc`", color=0x8400bb)
+        embed.add_field(name="Your link:", value="https://link-to.net/27527/pinterest", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+        
+    if message.content.startswith("!familyowner"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Spotify - family owner accs`", color=0xff8400)
+        embed.add_field(name="Your link:", value="https://direct-link.net/27527/familyowner", inline=False)
+        embed.add_field(name="Invite this bot on your server!", value="https://discordapfp.com/oauth2/authorize?client_id=60496724186339f7376&permissions=8&scope=bot", inline=False)
+        await message.author.send(embed=embed)
+
+    if message.content.startswith("!"):
+        print(message.author.name)
+        embed = discord.Embed(title="`Accounts sent to you DMs man!`", color=0xaeff84)
         await message.channel.send(embed=embed)
    
 @client.event
